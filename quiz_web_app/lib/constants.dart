fe1e688b3b24f02fdb5e9cd0dd813d573662af2e
valle0202 <leander@fritsch.de.com>
2021-02-04 11:56:38 +0100

    delete old version

--- quiz_web_app/lib/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const primaryBlue = Color(0xFF063B9E);
-const secondaryBlue = Color(0xFF0B78B8);
-const greenSuccess = Color(0xFF08875B);
-const yellowWarning = Color(0xFFFBCA32);
-const redDanger = Color(0xFFE4002B);
