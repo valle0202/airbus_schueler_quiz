83bb61d8455051e30a0b04f5028aebe64d1d8a08
valle0202 <leander@fritsch.de.com>
2021-02-17 20:15:57 +0100

    minor bug fix

--- schueler_quiz_web_app/lib/Screens/home_screen.dart
+++ schueler_quiz_web_app/lib/Screens/home_screen.dart
@@ -5,50 +5,10 @@ import 'package:flutter/widgets.dart';
 import 'package:schueler_quiz_web_app/constants.dart';
 
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    //final appName = 'Custom Themes';
-
-    return MaterialApp(
-      title: 'Airbus Schüler Quiz',
-      theme: ThemeData(
-        // Define the default brightness and colors.
-        //brightness: Brightness.dark,
-        primaryColor: primaryBlue,
-        highlightColor: highlightColor1,
-        accentColor: secondaryBlue,
-        backgroundColor: primaryBlue,
-        focusColor: primaryBlue,
-        errorColor: redDanger,
-
-        // Define the default font family.
-        fontFamily: 'Roboto',
-
-        // Define the default TextTheme. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
-        textTheme: TextTheme(
-          bodyText1: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.w300),
-          //headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          //headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          //bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-        ),
-      ),
-      home: HomeScreen(),
-    );
-  }
-}
-
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
-
-
 
 class _HomeScreenState extends State<HomeScreen> {
 
