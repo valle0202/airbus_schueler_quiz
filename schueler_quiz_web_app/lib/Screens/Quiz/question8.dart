7bc7c2bbc643f0a490bb55ebcfe62fb550beea40
dmasloub <david.masloub@googlemail.com>
2021-02-19 11:36:27 +0100

    question8

--- schueler_quiz_web_app/lib/Screens/Quiz/question8.dart
+++ schueler_quiz_web_app/lib/Screens/Quiz/question8.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 Widget question8(BuildContext context) {
-  return Text("frage8");
+  return Padding(
+    padding: const EdgeInsets.fromLTRB(100, 0, 100, 20),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Flexible(
+          flex: 1,
+          child: Text(
+            'The Look and Say Sequence',
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        ),
+        Flexible(
+            flex: 10,
+            child: Container(
+                child: Image.asset('assets/images/Aufgabe_3_Ver_1_Weiss.png'))),
+        Flexible(
+          flex: 1,
+          child: Text(
+            '1 | 11 | 21 | 1211 | 111221 | 312211 | 12112221 | . . . . . . . . . .',
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        )
+      ],
+    ),
+  );
 }
